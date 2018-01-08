922764ec35912b0aa4cf68e316b632ec1b7091cd
Mark Pinero <markspinero@gmail.com>
2018-01-08 11:47:34 -0800

    add em

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './app.css'
 class App extends Component {
   render() {
     return (
-      <div className="w-full max-w-2xl">
+      <div className="w-full max-w-2xl text-grey-darkest">
         <div className="hero max-w-sm mt-80">
           <h1 className="font-poppins">Hi, I'm Mark Pinero.</h1>
-          <p className="mt-8 leading-tight lg:text-2xl">I'm a full-stack developer from Los Angeles, CA. I specialize in front-end development with a keen eye for design and user experience. I'm currently seeking new opportunities.</p>
+          <p className="mt-8 leading-tight lg:text-2xl">I'm a full-stack developer from Los Angeles, CA. I specialize in front-end development with a keen eye for design and user experience. <span className="font-bold text-indigo">I'm currently seeking new opportunities.</span></p>
           <ul className="text-base leading-normal px-4 mt-8 md:text-lg md:px-0">
             <li><a className="no-underline border-b pb-px" href="https://linkedin.com/in/markpinero">Linkedin</a></li>
             <li><a className="no-underline border-b pb-px" href="https://github.com/remarks">Github</a></li>
