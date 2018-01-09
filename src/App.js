afccf7115a69c52782611990806fcc8211f6360e
Mark Pinero <markspinero@gmail.com>
2018-01-08 17:57:03 -0800

    add email link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="w-full max-w-2xl text-grey-darkest">
         <div className="hero max-w-sm mt-80">
           <h1 className="font-poppins">Hi, I'm Mark Pinero.</h1>
-          <p className="mt-8 leading-tight lg:text-2xl">I'm a full-stack developer from Los Angeles, CA. I specialize in front-end development with a keen eye for design and user experience. <span className="font-bold text-indigo">I'm currently seeking new opportunities.</span></p>
+          <p className="mt-8 leading-tight lg:text-2xl lg">I'm a full-stack developer from Los Angeles, CA. I specialize in front-end development with a keen eye for design and user experience. <a href="mailto:mark@markpinero.com" className="font-bold text-indigo no-underline border-b lg:inline-block">I'm currently seeking new opportunities.</a></p>
           <ul className="text-base leading-normal px-4 mt-8 md:text-lg md:px-0">
             <li><a className="no-underline border-b pb-px" href="https://linkedin.com/in/markpinero">Linkedin</a></li>
             <li><a className="no-underline border-b pb-px" href="https://github.com/remarks">Github</a></li>
