88fbd800f21bfbf2c823fef4905c16b92f0fb630
Mark Pinero <markspinero@gmail.com>
2018-05-01 23:39:03 -0700

    remove email link

--- src/Menu.js
+++ src/Menu.js
@@ -8,7 +8,6 @@ class Menu extends Component {
           <li className="pr-2 md:pb-2"><a className="no-underline border-b border-red pb-px" href="https://linkedin.com/in/markpinero">Linkedin</a></li>
           <li className="pr-2 md:pb-2"><a className="no-underline border-b border-red pb-px" href="https://github.com/remarks">Github</a></li>
           <li className="pr-2 md:pb-2"><a className="no-underline border-b border-red pb-px" href="/resume.pdf">Resume</a></li>
-          <li className="md:mt-8 md:-ml-4 md:pb-2"><a className="no-underline md:rounded md:bg-red border-b border-red md:border-none transition md:py-2 md:px-4 md:text-white hover:bg-indigo-darker hover:text-white" href="mailto:mark@markpinero.com">E-mail Me</a></li>
         </ul>
       </div>
     )
