3afdd481fbdcf2cc63a0bace08f00a0f4412617d
Mark Pinero <markspinero@gmail.com>
2018-05-19 14:11:57 -0700

    styling and email link

--- src/Hero.js
+++ src/Hero.js
@@ -6,10 +6,7 @@ class Hero extends Component {
       <div className="hero max-w-lg leading-normal">
         <h1 className="leading-tight font-extrabold">Hi, I'm Mark Pinero.</h1>
         <p className="mt-8 lg:text-2xl">
-          I'm a Full-Stack Developer from Los Angeles, CA, currently working as a Front End Developer for <a href="https://humnutrition.com" className="no-underline border-b border-red font-bold text-red" rel="nofollow">HUM Nutrition</a>. Working with creative agencies and various startups, I have a keen eye for user experience and design.
-        </p>
-        <p className="mt-8 lg:text-2xl">
-          <a href="mailto:mark@markpinero.com" className="no-underline font-bold border-b border-red">I'm available for part-time consulting or coffee.</a>
+          I'm a Full-Stack Developer from Los Angeles, CA, currently working as a Front End Developer for <a href="https://humnutrition.com" className="no-underline font-bold text-red" rel="nofollow">HUM Nutrition</a>. Working with creative agencies and various startups, I have a keen eye for user experience and design.
         </p>
       </div>
     )
--- src/Menu.js
+++ src/Menu.js
@@ -5,9 +5,10 @@ class Menu extends Component {
     return (
       <div className="sidebar">
         <ul className="list-reset flex md:block text-base leading-normal md:text-lg md:px-0">
-          <li className="pr-2 md:pb-2"><a className="no-underline border-b border-red pb-px" href="https://linkedin.com/in/markpinero">Linkedin</a></li>
-          <li className="pr-2 md:pb-2"><a className="no-underline border-b border-red pb-px" href="https://github.com/remarks">Github</a></li>
-          <li className="pr-2 md:pb-2"><a className="no-underline border-b border-red pb-px" href="/resume.pdf">Resume</a></li>
+          <li className="pr-2 md:pb-2"><a className="no-underline pb-px" href="https://linkedin.com/in/markpinero">Linkedin</a></li>
+          <li className="pr-2 md:pb-2"><a className="no-underline pb-px" href="https://github.com/remarks">Github</a></li>
+          <li className="pr-2 md:pb-6"><a className="no-underline pb-px" href="/resume.pdf">Resume</a></li>
+          <li className="pr-2 md:pb-2"><a className="no-underline pb-px" href="mailto:mark@markpinero.com">Email</a></li>
         </ul>
       </div>
     )
