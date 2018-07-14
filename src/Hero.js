67e329eb2ebf6be8755eaae2be8701c36fabac32
Mark Pinero <markspinero@gmail.com>
2018-07-14 00:15:20 -0700

    update copy

--- src/Hero.js
+++ src/Hero.js
@@ -6,7 +6,7 @@ class Hero extends Component {
       <div className="hero max-w-lg leading-normal">
         <h1 className="leading-tight font-extrabold">Hi, I'm Mark Pinero.</h1>
         <p className="mt-8 lg:text-2xl">
-          I'm a Full-Stack Developer from Los Angeles, CA, currently working as a Front End Developer for <a href="https://humnutrition.com" className="no-underline font-bold text-red" rel="nofollow">HUM Nutrition</a>. Working with creative agencies and various startups, I have a keen eye for user experience and design.
+          I'm a Front End Developer from Los Angeles, CA, currently working for <a href="https://humnutrition.com" className="no-underline font-bold text-red" rel="nofollow">HUM Nutrition</a>. Working with creative agencies and various startups, I've developed a keen eye for user experience and design.
         </p>
       </div>
     )
