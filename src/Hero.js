50f536d4c416f89e7a502d023e0b6264501c3960
Mark Pinero <markspinero@gmail.com>
2019-04-12 11:19:41 -0700

    fixes

--- src/Hero.js
+++ src/Hero.js
@@ -6,8 +6,9 @@ class Hero extends Component {
       <div className="hero max-w-md leading-normal">
         <h1 className="leading-tight font-extrabold">Hi, I'm Mark Pinero.</h1>
         <p className="mt-8 lg:text-2xl">
-          I'm a Front End Developer from Los Angeles, CA, currently working for <a href="https://humnutrition.com" className="no-underline font-bold text-red" rel="nofollow">HUM Nutrition</a>. Working with creative agencies and various startups, I've developed a keen eye for user experience and design.
+          I'm a Front End Developer specializing in Vue.js, currently working for <a href="https://humnutrition.com" className="no-underline font-bold text-red" rel="nofollow">HUM Nutrition</a> in Los Angeles, CA.
         </p>
+        <p className="mt-4 lg:text-2xl">Working with creative agencies and various startups, I've developed a keen eye for user experience and design.</p>
         <p className="mt-8 lg:text-2xl">
           <a href="mailto:mark@markpinero.com">I'm available for consulting</a>.
         </p>
--- src/Projects.js
+++ src/Projects.js
@@ -6,36 +6,24 @@ const Project = item => {
   const projectImageStyle = `border-${item.style} w-full border-8 border-solid block`
   const linkStyle = `no-underline text-${item.style}`
   return (
-    <div className="my-8">
+    <div className="mt-8">
       <Link to={item.url} rel="nofollow">
         <img src={item.image} className={projectImageStyle} />
       </Link>
-      <h2 className="mt-4 md:mt-8 text-xl md:text-2xl leading-normal">
+      <h2 className="mt-4 text-xl md:text-2xl leading-normal">
         <Link to={item.url} rel="nofollow" className={linkStyle}>
           {item.title}
         </Link>
       </h2>
-      <div className="text-base md:text-lg leading-normal">
-        <div>
-          <strong>Stack: </strong>
-          <span class="stack-item">
-            {item.stack.slice(0, -1).join(', ')}, {item.stack.slice(-1)[0]}
-          </span>
-        </div>
-        <div className="mt-4">{item.desc}</div>
-      </div>
     </div>
   )
 }
 
-const renderProjects = projects => {
-  return projects.map(project => <Project key={project.url} {...project} />)
-}
+const renderProjects = projects => projects.map(project => <Project key={project.url} {...project} />)
 
 class Projects extends React.Component {
   render() {
     const work = renderProjects(data.filter(x => x.type === 'work'))
-    const personal = renderProjects(data.filter(x => x.type === 'personal'))
 
     return (
       <div className="portfolio">
