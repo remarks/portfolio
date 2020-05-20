e2a95a22ddf7a71868d049adf5c99026cc70fb2e
Mark Pinero <markspinero@gmail.com>
2020-05-19 22:56:22 -0700

    fix: adjust type

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="container space-y-8 md:space-y-16">
-    <header id="introduction" class="max-w-2xl space-y-4 text-xl leading-snug lg:text-2xl lg:space-y-6">
+    <header id="introduction" class="max-w-2xl space-y-4 text-xl leading-snug tracking-tight lg:text-2xl lg:space-y-6">
       <h1 class="text-4xl font-extrabold leading-none tracking-tight lg:text-5xl">
         Hi, I'm Mark Pinero.
       </h1>
       <p>
         I'm a Senior Front End Developer specializing in <strong>Vue.js</strong>, currently working for
-        <a href="https://humnutrition.com" class="font-extrabold" rel="noopener">
+        <a href="https://humnutrition.com" class="font-bold" rel="noopener">
           HUM Nutrition
         </a>
         in Los Angeles, CA.
